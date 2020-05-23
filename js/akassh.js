4573e95e754772a58f08471db941a286910b4b78
unknown <akassh.shah77@gmail.com>
2020-05-23 17:47:22 -0400

    added better coming soon page and working on main page

--- js/akassh.js
+++ js/akassh.js
@@ -1,3 +1,17 @@
+// $(document).ready(function () {
+//   $(".nav li a").removeClass("active");//this will remove the active class from  
+//   // //previously active menu item 
+//   // $('#home').addClass('active');
+//   // // for demo
+//   // $('#demo').addClass('active');
+//   // // for sale 
+//   // $('#sale').addClass('active');
+// });
+
 $(document).ready(function () {
-    $(".full-screen-scroller").fullScreenScroller();
+  var url = window.location;
+  $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+  $('ul.nav a').filter(function() {
+       return this.href == url;
+  }).parent().addClass('active');
 });
